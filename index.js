87475c8228367b4f72e012d1f618b7082dfd4ad9
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-04-30 12:29:52 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 /* write the code to run app.js here */
+const app = require("./app");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`app listening on ${PORT}`);
+});
