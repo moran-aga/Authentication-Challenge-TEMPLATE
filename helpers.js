5afc64bc9064bf967898625b0c6899b0234637d6
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-04-30 12:30:03 +0300

    all test pass

---
+++ helpers.js
@@ -0,0 +1,12 @@
+const USERS = [
+  {
+    email: "admin@email.com",
+    name: "admin",
+    password: "$2b$10$jRxvyE7qrh7w5.LGBX0bh.CPs5S4sihySoWO4g.jzmjMlJQr4jJ6G",
+    isAdmin: true,
+  },
+];
+const INFORMATION = [];
+const REFRESHTOKENS = [];
+
+module.exports = { USERS, INFORMATION, REFRESHTOKENS };
